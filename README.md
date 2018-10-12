079f9bfae8ac0bbd92f87a1ab41e439634084c44
Michele Boggia <maicol.boggia@gmail.com>
2018-10-12 11:52:07 +0200

    Writing analysis code. Got to the point where I have a huge dataframe witl all the info. Added in readme some info about the starting dataset

--- README.md
+++ README.md
@@ -9,7 +9,53 @@ Starting dataset downloaded from [here](https://d396qusza40orc.cloudfront.net/ge
 
 ## ./data
 
-Folder containing unzipped data before cleaning
+Folder containing unzipped data, before cleaning. The folder contains two datasets:
+
+* *data/test*,
+* *data/train*.
+
+and some additional files
+
+* *README.txt*
+* *activity_labels.txt*
+* *features.txt*
+* *features_info.txt*
+
+### Datasets
+
+The train dataset contains:
+
+* *X_train.txt*
+The actual dataset, in a 7352x561 table. Each row corresponds to an observation (window sample), each column to a feature.
+
+* *y_train.txt*
+Contains labels specifying the type of activity for each observation. It is a vector of length 7352, with entries in the range 1-6, which correspond to the six types of activity considered.
+
+* *subject_train.txt*
+Contains a vector of length 7352, with entries in the range 1-30, corresponding to the individual performing the activity for each observation.
+
+* *Inertial Signals (folder)*
+Contains the raw data used to compute the 561 features reported in *X_train.txt*. Contains various tables of dim 7352x128. 128 is the number of numerical output obtained from the device in each observations for body acceleration, angular accelerations, (...) used to compute the features. 
+
+The test dataset has a similar structure, with the following differences:
+
+* The observations are only 2947, and correspond to the observations taken on the 30 individals selected randomly to build the test dataset.
+
+* The features considered, as well as the activity types are the same as for the train dataset.
+
+### Other files
+
+* *README.txt*
+Readme file, with more info.
+
+* *activity_labels.txt*
+Links between the indices 1-6 to hte activity type (walking, ...)
+
+* *features.txt*
+Labels for the 561 columns of the tables reported in *X_train.txt* and *X_test.xt*.
+
+* *features_info.txt*
+More info about the features and how they were computed.
 
 ## ./run_analysis.R
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,84 @@
-# preliminaries
-# rm(list = ls())
-# setwd("/home/miche/Repositories/getCleanDataProject")
+## preliminaries
+rm(list = ls())
+setwd("/home/miche/Repositories/getCleanDataProject")
 
-# I download the data
+## loading packages
+library("dplyr")
+
+## I download and unzip the data
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 if(!file.exists("dataset.zip")){
     download.file(fileURL,destfile = "dataset.zip")
 }
-# and unzip, putting content in ./data
 if(!file.exists("./data")){
     dir.create("data")
     unzip("dataset.zip",exdir = "./data")
 }
 
+## Firstly, I merge the train and test datsets. In order to keep as much
+## information as possible, before merging, I add a column specifying the
+## original dataset (training or test)
+
+# load files with info ad giving names
+list.Features <- read.table("data/UCI HAR Dataset/features.txt")
+list.Features <- rename(list.Features,"featureid" = "V1","featurename" = "V2")
+list.Activities <- read.table("data/UCI HAR Dataset/activity_labels.txt")
+list.Activities <- rename(list.Activities,
+                          "activityid" = "V1",
+                          "activityname" = "V2"
+                          )
+
+# load the training dataset
+data.Train.X <- read.table("data/UCI HAR Dataset/train/X_train.txt")
+data.Train.Y <- read.table("data/UCI HAR Dataset/train/y_train.txt")
+data.Train.Subj <- read.table("data/UCI HAR Dataset/train/subject_train.txt")
+# load the test dataset
+data.Test.X <- read.table("data/UCI HAR Dataset/test/X_test.txt")
+data.Test.Y <- read.table("data/UCI HAR Dataset/test/y_test.txt")
+data.Test.Subj <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
+
+# Setting names to dataframes of training set
+names(data.Train.X) <- list.Features$featurename 
+data.Train.Y <- rename(data.Train.Y,"activity" = "V1")
+data.Train.Subj <- rename(data.Train.Subj,"subjectid" = "V1")
+
+
+# Setting names to dataframes of test set
+names(data.Test.X) <- list.Features$featurename 
+data.Test.Y <- rename(data.Test.Y,"activity" = "V1")
+data.Test.Subj <- rename(data.Test.Subj,"subjectid" = "V1")
+
+# Storing dimensions of dataframes
+nobs.Train <- nrow(data.Train.X) # number of window samples in training dataset
+nobs.Test <- nrow(data.Test.X)   # number of window samples in test dataset
+nobs.Global <- nobs.Train + nobs.Test # total number of window samples
+
+# Adding a column to keep track of original dataset
+data.Train.X$dataset <- as.factor(rep("train",nobs.Train))
+data.Test.X$dataset <- as.factor(rep("test",nobs.Test))
+
+# I merge the two datasets (training BEFORE test in rows)
+data.Global.X <- rbind(data.Train.X,data.Test.X)
+data.Global.Y <- rbind(data.Train.Y,data.Test.Y)
+data.Global.Subj <- rbind(data.Train.Subj,data.Test.Subj)
+
+# I specify the activities using factors and with levels given by proper names
+data.Global.Y$activity <- as.factor(data.Global.Y$activity)
+levels(data.Global.Y$activity) <- list.Activities$activityname
+
+# I specify the subjects using factors instead of integers
+data.Global.Subj$subjectid <- as.factor(data.Global.Subj$subjectid)
+
+# I put all informations together in a dataframe data.Global
+# This dataframe contains as much informations as possible coming from the
+# starting data and can be used to extract what is needed for the project.
+data.Global <- data.frame(
+    activity = data.Global.Y$activity,
+    subjectid = data.Global.Subj$subjectid,
+    data.Global.X
+)
+
+# TODOs: 
+#  - set units
+#  - select required data
+#  - build second dataset with required average values
