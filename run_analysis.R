4d16d5b8c9a1020d69f19bb4fffa76c037825464
Michele Boggia <maicol.boggia@gmail.com>
2018-10-13 15:45:21 +0200

    Changed first commands so that the script can be executed in other directories

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,18 @@
+## Getting and Cleaning Data Project John Hopkins Coursera
+## Author: Michele Boggia
+
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each
+##    measurement.
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names.
+## 5. From the data set in step 4, creates a second, independent tidy data set
+##    with the average of each variable for each activity and each subject.
+
 ## preliminaries
-rm(list = ls())
-setwd("/home/miche/Repositories/getCleanDataProject")
+#rm(list = ls())
+#setwd("/home/miche/Repositories/getCleanDataProject")
+
 
 ## loading packages
 library("dplyr")
